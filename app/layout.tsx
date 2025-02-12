c74406c25e94439efd97272bc47e8d63627c71fb
Nowath <nanoprocom@gmail.com>
2025-02-12 09:22:59 +0700

    title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,20 +5,20 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar/Navbars";
 
 const kanit = Kanit({
-  weight: ["100", "200", "400", "600"],
-  subsets: ["latin"],
+    weight: ["100", "200", "400", "600"],
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "MANGALAND - อ่านมังงะแปลไทย อัปเดตไวที่สุด!",
-  description:
-    "MANGALAND คือเว็บไซต์อ่านการ์ตูนออนไลน์ที่ออกแบบมาให้ใช้งานง่าย สะดวก รวดเร็ว พร้อมอัปเดตมังงะใหม่ทุกวัน! พบกับมังงะหลากหลายแนว ทั้งแอ็กชัน โรแมนซ์ แฟนตาซี และอีกมากมาย ทุกเรื่องแปลไทยให้อ่านฟรี คุณภาพคมชัด โหลดไว ไม่พลาดทุกตอนสำคัญ!",
+    title: "MANGALAND - อ่านมังงะแปลไทย อัปเดตไวที่สุด!",
+    description:
+        "MANGALAND คือเว็บไซต์อ่านการ์ตูนออนไลน์ที่ออกแบบมาให้ใช้งานง่าย สะดวก รวดเร็ว พร้อมอัปเดตมังงะใหม่ทุกวัน! พบกับมังงะหลากหลายแนว ทั้งแอ็กชัน โรแมนซ์ แฟนตาซี และอีกมากมาย ทุกเรื่องแปลไทยให้อ่านฟรี คุณภาพคมชัด โหลดไว ไม่พลาดทุกตอนสำคัญ!",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
     return (
         <html lang="en">
