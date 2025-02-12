cd18e0ff6d055b7260e5bee8d1b97b5144227371
Pacharapon <sundaynight.1919@gmail.com>
2025-02-12 22:09:30 +0700

    fix: Type erorr on build

--- app/tags/[slug]/page.tsx
+++ app/tags/[slug]/page.tsx
@@ -1,3 +1,7 @@
-export default function Tag({ params }: { params: { slug: string } }) {
-    return <div>Tag: {params.slug}</div>;
+type Params = Promise<{ slug: string }>;
+
+export default async function Tag({ params }: { params: Params }) {
+    const { slug } = await params;
+
+    return <div>Tag: {slug}</div>;
 }
