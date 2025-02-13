4089f5ab84ad4037efa56aa7b1ecc58089953358
Pacharapon <sundaynight.1919@gmail.com>
2025-02-14 03:00:22 +0700

    optimize: ssr

--- app/(guest)/page.tsx
+++ app/(guest)/page.tsx
@@ -1,41 +1,16 @@
-"use client";
-import { ChevronUpIcon } from "@heroicons/react/20/solid";
-import { useState, useEffect } from "react";
 import Advertise from "../components/Advertise/Advertise";
+import { ScrollUp } from "../components/Footer/ScrollUp";
 
 export default function Home() {
-    const [isVisible, setIsVisible] = useState(false);
-
-    useEffect(() => {
-        const toggleVisibility = () => {
-            if (window.scrollY > 200) {
-                setIsVisible(true);
-            } else {
-                setIsVisible(false);
-            }
-        };
-
-        window.addEventListener("scroll", toggleVisibility);
-        return () => window.removeEventListener("scroll", toggleVisibility);
-    }, []);
-
-    const scrollToTop = () => {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-    };
     return (
         <div>
             <Advertise />
+
             <div>
                 <h2>🔥 5 อันดับมังงะยอดฮิตประจำเดือนนี้</h2>
             </div>
-            <button
-                onClick={scrollToTop}
-                className={`${
-                    isVisible ? `opacity-100` : `opacity-0`
-                } fixed right-10 z-50 bottom-10 md:right-20 md:bottom-20 bg-pink w-10 h-10 rounded-full Arrow`}
-            >
-                <ChevronUpIcon className="size-10" />
-            </button>
+            
+            <ScrollUp />
         </div>
     );
 }
---
+++ app/components/Footer/Scrollup.tsx
@@ -0,0 +1,31 @@
+import { ChevronUpIcon } from "@heroicons/react/24/outline";
+import { useEffect, useState } from "react";
+
+export const ScrollUp = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        const toggleVisibility = () => {
+            if (window.scrollY > 200) {
+                setIsVisible(true);
+            } else {
+                setIsVisible(false);
+            }
+        };
+
+        window.addEventListener("scroll", toggleVisibility);
+        return () => window.removeEventListener("scroll", toggleVisibility);
+    }, []);
+
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    };
+
+    return <button
+        onClick={scrollToTop}
+        className={`${isVisible ? `opacity-100` : `opacity-0`
+            } fixed right-10 z-50 bottom-10 md:right-20 md:bottom-20 bg-pink w-10 h-10 rounded-full Arrow`}
+    >
+        <ChevronUpIcon className="size-10" />
+    </button>
+}
